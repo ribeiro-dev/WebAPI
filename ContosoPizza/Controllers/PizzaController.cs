dd3b8bff742ce28d12f90872e9de7f66f8b9b465
rafaelg <romilson.2r@gmail.com>
2021-05-14 14:35:32 -0300

    Add a controller

---
+++ ContosoPizza/Controllers/PizzaController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+
+namespace ContosoPizza.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] // controller will get the name of the class (pizza)
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza is null)
+                return NotFound();
+
+            return pizza;
+        }
+
+        // POST action
+
+        // PUT action
+
+        // DELETE action
+    }
+}
